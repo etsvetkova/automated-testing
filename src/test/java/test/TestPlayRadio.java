046430e23fe5b6c1faee8b44e2f19606503be60c
etsvetkova <etsvetkova97@gmail.com>
2019-06-13 15:27:49 +0300

    Radio play test fixed

--- src/test/java/test/TestPlayRadio.java
+++ src/test/java/test/TestPlayRadio.java
@@ -1,17 +1,19 @@
 package test;
 
 import MainPrigect.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestPlayRadio extends TestBase {
     @Test
-    public void TestPlayRadio() {
-
+    public void testPlayRadio() {
         LoginPage loginPage = new LoginPage(driver);;
         UserMainPage userMainPage = loginPage.login();
         MusicPage musicPage = userMainPage.clickToMusic();
         RadioPage radioPage = musicPage.clickRadio();
         radioPage.clickOnButtonPlay();
-        radioPage.checkVisibleButtonsPause();
+        Assert.assertTrue("Нет кнопки паузы на контроллере", radioPage.checkVisibleControlsButtonPause());
+        Assert.assertTrue("Нет кнопки паузы на альбоме", radioPage.checkVisibleAlbumButtonPause());
+        Assert.assertTrue("Нет кнопки паузы на песне", radioPage.checkVisibleButtonPause());
     }
 }
